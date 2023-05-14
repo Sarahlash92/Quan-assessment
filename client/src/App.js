548f6929a0a58ed22a54b4759e09ffb3cf37d989
Sarah Lash <sarah.lash92@gmail.com>
2023-05-14 14:44:48 +0200

    Setting up the main Theme
    - Setting up folder structure
    - Set up main theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import "./App.css";
 
-import './App.css';
+/* Setting up 
+the main Theme */
+
+const quanTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#130a29",
+    },
+    secondary: {
+      main: "#362a51",
+    },
+    text: {
+      primary: "#ffff",
+      secondary: "#ffcd35",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider theme={quanTheme}>
+      <CssBaseline />
+      <div className="App"></div>
+    </ThemeProvider>
   );
 }
 
